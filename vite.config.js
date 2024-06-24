b99aa33c8635ac85bd159e80d5269765b89ca897
glenn <grdimaano18@gmail.com>
2024-06-24 14:14:09 +0800

    initial vite and tailwind setup

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+})
